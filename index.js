7dfcb6186a244da1a4b7328b0cab1c58255f813a
Jesse Ma <j.masonoma17@gmail.com>
2018-07-22 13:58:47 -0700

    Require todos from routes folder, configure app to use the imported todos

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+const toDoRoutes = require('./routes/todos');
+
+app.get('/', (request, response) => response.send('Welcome to the Root route!'));
+
+app.use('/api/todos', toDoRoutes);
+
 const port = 8000;
 
 app.listen(port, function() {
