c0463dac40c5bdca5e219a064532d293aed8f037
Jesse Ma <j.masonoma17@gmail.com>
2018-07-22 14:09:22 -0700

    Update get route for api/todos to retrieve documents from db

--- routes/todos.js
+++ routes/todos.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models');
 
-router.get('/', (request, response) => response.send('Hello, testing'));
+router.get('/', (request, response) => {
+    db.Todo.find()
+    .then((todos) => {
+        response.json(todos);
+    })
+    .catch((error) => {
+        response.send(error);
+    })
+});
 
 module.exports = router;
