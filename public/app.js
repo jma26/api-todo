be35cc8bf1313e1ea80a5c8cdbb27728d02dcf28
Jesse Ma <j.masonoma17@gmail.com>
2018-07-24 18:53:55 -0700

    Add addTodo() to populate the list instead of addTodos(), add keypress event to call createTodo()

--- public/app.js
+++ public/app.js
@@ -2,14 +2,32 @@ $(document).ready(function() {
     $.getJSON('/api/todos')
     .then(addTodos)
     .catch((error) => response.send(error))
+
+    $('#todoInput').keypress((event) => {
+        if (event.which === 13) {
+            createTodo();
+        }
+    })
 });
 
-function addTodos(todos) {
-    todos.forEach((todo) => {
-        let newTodo = $(`<li class="task"> ${todo.task} - ${todo.completed} </li>`)
+function createTodo() {
+    let userInput = $('#todoInput').val();
+    $.post('/api/todos', {task: userInput})
+    .then((newTodo) => addTodo(newTodo))
+    .catch((error) => response.send(error));
+}
+
+function addTodo(todo) {
+    $('#todoInput').val('');
+    let newTodo = $(`<li class="task"> ${todo.task} - ${todo.completed} </li>`)
         if (todo.completed) {
             newTodo.addClass('done');
         }
         $('.list').append(newTodo);
+}
+
+function addTodos(todos) {
+    todos.forEach((todo) => {
+        addTodo(todo);
     })
 }
