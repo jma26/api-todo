695a82a4b00c2e7d8e531df67401161e3393712e
Jesse Ma <j.masonoma17@gmail.com>
2018-07-24 18:39:10 -0700

    Add an if statement to add class, .done, to todos that are completed

--- public/app.js
+++ public/app.js
@@ -1,1 +1,15 @@
-alert('this is a test');
+$(document).ready(function() {
+    $.getJSON('/api/todos')
+    .then(addTodos)
+    .catch((error) => response.send(error))
+});
+
+function addTodos(todos) {
+    todos.forEach((todo) => {
+        let newTodo = $(`<li class="task"> ${todo.task} - ${todo.completed} </li>`)
+        if (todo.completed) {
+            newTodo.addClass('done');
+        }
+        $('.list').append(newTodo);
+    })
+}
